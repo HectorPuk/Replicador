27a339c2c12a4712bb761a5d49f9b092aca957c8
HectorPuk <hector.puk@gmail.com>
2021-08-15 13:14:11 -0300

    Update WireShark.sql

--- WireShark.sql
+++ WireShark.sql
@@ -1,1 +1,5 @@
 -- Capture Filters .... https://wiki.wireshark.org/CaptureFilters
+--Asi crea el usuario que va a usar SAP B1.
+--CREATE USER B1_44534C50524F44554332_RW PASSWORD "i%+M<^+b/pPw%i8y803Q29fLR4GX1"
+--La creacion se realiza despues del import y cuando alguien trata de conectarse.
+
