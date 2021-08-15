0def0d0662bd36cf02bcdfe925f19a347e5f27df
HectorPuk <hector.puk@gmail.com>
2021-08-15 13:15:30 -0300

    Update WireShark.sql

--- WireShark.sql
+++ WireShark.sql
@@ -1,4 +1,5 @@
 -- Capture Filters .... https://wiki.wireshark.org/CaptureFilters
+-- Ejemplo de un CAPTURE FILTER: dst port 30015 and (host 192.168.1.227 or host 192.168.1.228)
 --Asi crea el usuario que va a usar SAP B1.
 --CREATE USER B1_44534C50524F44554332_RW PASSWORD "i%+M<^+b/pPw%i8y803Q29fLR4GX1"
 --La creacion se realiza despues del import y cuando alguien trata de conectarse.
