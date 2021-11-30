d756946d08f6f5fe191a5f110fb4a52a8b590dda
HectorPuk <hector.puk@gmail.com>
2021-11-30 14:32:55 -0300

    Create ElDeclareY-LPMQLRemilP.sql

---
+++ ElDeclareY-LPMQLRemilP.sql
@@ -0,0 +1,7 @@
+DO 
+BEGIN
+DECLARE SAMPLE INT = 1;
+SELECT SAMPLE from DUMMY;
+END;
+--Nota para ver el tema de comentarios mira la parte de Anonymous Block
+--En SAP B1 Query manager abstraete del DO. EL DO NO VA OJO!!
