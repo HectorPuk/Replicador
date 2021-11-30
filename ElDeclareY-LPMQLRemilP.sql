f27b80535916e158af18cebd565aecf7f9861b39
HectorPuk <hector.puk@gmail.com>
2021-11-30 16:38:02 -0300

    Update ElDeclareY-LPMQLRemilP.sql

--- ElDeclareY-LPMQLRemilP.sql
+++ ElDeclareY-LPMQLRemilP.sql
@@ -5,3 +5,5 @@ SELECT SAMPLE from DUMMY;
 END;
 --Nota para ver el tema de comentarios mira la parte de Anonymous Block
 --En SAP B1 Query manager abstraete del DO. EL DO NO VA OJO!!
+--Tipos de Variable DATE | TIME| TIMESTAMP | SECONDDATE | TINYINT | SMALLINT | INTEGER | BIGINT | DECIMAL | SMALLDECIMAL | REAL | DOUBLE
+--| VARCHAR | NVARCHAR | ALPHANUM | VARBINARY | CLOB | NCLOB | BLOB
