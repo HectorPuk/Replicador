a5eabf054f6a6b8ac5b41da3673e649411264d43
HectorPuk <hector.puk@gmail.com>
2022-02-21 07:50:02 -0300

    Create HANACatalog.sql

---
+++ HANACatalog.sql
@@ -0,0 +1,12 @@
+select * from M_backup_catalog 
+where ENTRY_TYPE_NAME = 'complete data backup' 
+order by backup_id desc;
+BACKUP CATALOG DELETE ALL BEFORE BACKUP_ID 
+1645229824168
+COMPLETE; 
+select count(*) from M_backup_catalog 
+where ENTRY_TYPE_NAME = 'complete data backup' 
+order by backup_id desc;
+BACKUP CATALOG DELETE ALL BEFORE BACKUP_ID 
+1645229824168
+COMPLETE; 
