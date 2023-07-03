7588f7bee268cf85498a8f6a84bbda11981ab3af
Hector Albino Puk <hector.puk@gmail.com>
2023-07-03 15:26:26 -0300

    Shells de Backup

---
+++ SYSTEMDB-backup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+rm /usr/sap/NDB/HDB00/backup/data/SYSTEMDB/COMPLETE*;
+rm /usr/sap/NDB/HDB00/backup/log/SYSTEMDB/log* ; 
+/usr/sap/hdbclient/hdbsql -u SYSTEM  -p 'Kaiser$641' -o /tmp/resulta -d SYSTEMDB 'do begin declare BKP nvarchar(30); execute immediate '"'"'BACKUP DATA USING FILE ('"'"''"'"'COMPLETE'"'"''"'"')'"'"'; select * from M_BACKUP_CATALOG order by UTC_END_TIME desc; select top 1 BACKUP_ID into BKP from M_BACKUP_CATALOG where ENTRY_TYPE_NAME ='"'"'complete data backup'"'"' and STATE_NAME = '"'"'successful'"'"' order by UTC_END_TIME desc; execute immediate '"'"'BACKUP CATALOG DELETE ALL BEFORE BACKUP_ID '"'"' || :BKP || '"'"' COMPLETE'"'"'; select * from M_BACKUP_CATALOG order by UTC_END_TIME desc; end;'
+
+
