05f43a74a80f36a2423f74f0ca9f454e05d01c62
HectorPuk <hector.puk@gmail.com>
2023-07-03 12:01:26 -0300

    Update #BackupHANA2Version2.sql

--- #BackupHANA2Version2.sql
+++ #BackupHANA2Version2.sql
@@ -21,3 +21,6 @@ Para conectarse como SYSTEM en SYSTEMDB desde un remoto
 ./hdbsql -u SYSTEM  -d SYSTEMDB -p 'Kaiser$641' -n 192.168.1.227:30013
 
 */
+
+
+rm /usr/sap/NDB/HDB00/backup/data/SYSTEMDB/COMPLETE* && rm /usr/sap/NDB/HDB00/backup/log/SYSTEMDB/COMPLETE* && /usr/sap/hdbclient/hdbsql -u SYSTEM  -p 'Kaiser$641' -o /tmp/resulta -d SYSTEMDB 'do begin declare BKP nvarchar(30); execute immediate '"'"'BACKUP DATA USING FILE ('"'"''"'"'COMPLETE'"'"''"'"')'"'"'; select * from M_BACKUP_CATALOG order by UTC_END_TIME desc; select top 1 BACKUP_ID into BKP from M_BACKUP_CATALOG where ENTRY_TYPE_NAME ='"'"'complete data backup'"'"' and STATE_NAME = '"'"'successful'"'"' order by UTC_END_TIME desc; execute immediate '"'"'BACKUP CATALOG DELETE ALL BEFORE BACKUP_ID '"'"' || :BKP || '"'"' COMPLETE'"'"'; select * from M_BACKUP_CATALOG order by UTC_END_TIME desc; end;'
