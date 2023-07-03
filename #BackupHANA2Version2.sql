e509271c42c591ad26e1f6bf2e416b475f43c63c
HectorPuk <hector.puk@gmail.com>
2023-07-03 14:41:34 -0300

    Update #BackupHANA2Version2.sql

--- #BackupHANA2Version2.sql
+++ #BackupHANA2Version2.sql
@@ -24,3 +24,11 @@ Para conectarse como SYSTEM en SYSTEMDB desde un remoto
 
 
 rm /usr/sap/NDB/HDB00/backup/data/SYSTEMDB/COMPLETE* && rm /usr/sap/NDB/HDB00/backup/log/SYSTEMDB/COMPLETE* && /usr/sap/hdbclient/hdbsql -u SYSTEM  -p 'Kaiser$641' -o /tmp/resulta -d SYSTEMDB 'do begin declare BKP nvarchar(30); execute immediate '"'"'BACKUP DATA USING FILE ('"'"''"'"'COMPLETE'"'"''"'"')'"'"'; select * from M_BACKUP_CATALOG order by UTC_END_TIME desc; select top 1 BACKUP_ID into BKP from M_BACKUP_CATALOG where ENTRY_TYPE_NAME ='"'"'complete data backup'"'"' and STATE_NAME = '"'"'successful'"'"' order by UTC_END_TIME desc; execute immediate '"'"'BACKUP CATALOG DELETE ALL BEFORE BACKUP_ID '"'"' || :BKP || '"'"' COMPLETE'"'"'; select * from M_BACKUP_CATALOG order by UTC_END_TIME desc; end;'
+
+
+#!/bin/sh
+rm /usr/sap/NDB/HDB00/backup/data/DB_NDB/COMPLETE*;
+rm /usr/sap/NDB/HDB00/backup/log/DB_NDB/log* ;
+/usr/sap/hdbclient/hdbsql -u SYSTEM  -p 'Kaiser$641' -o /tmp/resulta -d NDB 'do begin declare BKP nvarchar(30); execute immediate '"'"'BACKUP DATA USING FILE ('"'"''"'"'COMPLETE'"'"''"'"')'"'"'; select * from M_BACKUP_CATALOG order by UTC_END_TIME desc; select top 1 BACK
+UP_ID into BKP from M_BACKUP_CATALOG where ENTRY_TYPE_NAME ='"'"'complete data backup'"'"' and STATE_NAME = '"'"'successful'"'"' order by UTC_END_TIME desc; execute immediate '"'"'BACKUP CATALOG DELETE ALL BEFORE BACKUP_ID '"'"' || :BKP || '"'"' COMPLETE'"'"'; select * f
+rom M_BACKUP_CATALOG order by UTC_END_TIME desc; end;'
