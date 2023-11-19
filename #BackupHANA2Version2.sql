6f664686b807818e254c888ab7aab3eef8431737
HectorPuk <hector.puk@gmail.com>
2023-11-18 22:48:50 -0300

    Update #BackupHANA2Version2.sql

--- #BackupHANA2Version2.sql
+++ #BackupHANA2Version2.sql
@@ -41,6 +41,51 @@ execute immediate 'BACKUP CATALOG DELETE ALL BEFORE BACKUP_ID ' || :BKP || ' COM
 end;"
 
 
+CRONTAB ejecutar a 12:55 y 22:55
+
+55 12,22 * * * /root/backupSYSTEMDB.sh
+55 12,22 * * * /root/backupNDB.sh
+
+backupSYSTEMDB.sh
+
+current_time=$(date "+%Y%m%d%H%M%S") && /hana/shared/NDB/hdbclient/hdbsql -u SYSTEM  -d SYSTEMDB  -p 'Ivana$641' -O /tmp/backupSYSTEMDBresult.$current_time \
+        "do \
+        begin \
+        declare BKP nvarchar(30);
+        execute immediate 'BACKUP DATA USING FILE (''COMPLETESYSTEMDB$current_time'')';
+        select BACKUP_ID into BKP from M_BACKUP_CATALOG where ENTRY_TYPE_NAME ='complete data backup' and STATE_NAME = 'successful' order by UTC_END_TIME desc limit 1 offset 2;
+        execute immediate 'BACKUP CATALOG DELETE ALL BEFORE BACKUP_ID ' || :BKP || ' COMPLETE';
+end;"
+
+~
+
+
+
+
+backupNDB.sh
+
+current_time=$(date "+%Y%m%d%H%M%S") && /hana/shared/NDB/hdbclient/hdbsql -u SYSTEM  -d NDB -p 'Ivana$641' -O /tmp/backupNDBresult.$current_time \
+        "do \
+        begin \
+        declare BKP nvarchar(30);
+        execute immediate 'BACKUP DATA USING FILE (''COMPLETENDB$current_time'')';
+        select BACKUP_ID into BKP from M_BACKUP_CATALOG where ENTRY_TYPE_NAME ='complete data backup' and STATE_NAME = 'successful' order by UTC_END_TIME desc limit 1 offset 2;
+        execute immediate 'BACKUP CATALOG DELETE ALL BEFORE BACKUP_ID ' || :BKP || ' COMPLETE';
+end;"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 */
 
 
